a68b5b6b865e0cd08c4b9647bd168f254a9736e8
Taha <>
2022-03-19 15:15:52 +0100

    DOM

---
+++ dom/script.js
@@ -0,0 +1,12 @@
+var inp1 = document.querySelector("#nb1");
+var inp2 = document.querySelector("#nb2");
+var btn = document.querySelector("#btn");
+var result = document.querySelector("#result");
+
+btn.addEventListener("click", function(event) {
+    event.preventDefault();
+    
+    var total = parseInt(inp1.value) + parseInt(inp2.value);
+    result.innerHTML = total;
+    result.style.color = total < 10 ? "red" : "green";
+})
