ac4b56655819ee2920a689815f393d077d003d3c
Taha <>
2022-03-24 23:40:18 +0100

    Step 1

---
+++ ratt/step1.js
@@ -0,0 +1,46 @@
+var a = 10;
+let b = 20;
+const c = 30;
+
+// var => fonction
+// let => bloc (if, for, while)
+
+/*
+ caractere
+ _
+ $
+ */
+
+function test(age) {
+    var a = 10;
+
+    if (age > 18) {
+        var b = 20;
+        let c = 10;
+    }
+}
+
+// Types primitifs (par valeur)
+/**
+ Nombre
+ String
+ Boolean
+ */
+
+// objets (par référence)
+
+var d = 10;
+var e = d;
+d = 20;
+
+var baseUser = {
+    nom: null,
+    age: null
+};
+
+var user = baseUser;
+
+user.nom = "Aziz";
+
+console.log("BASE USER", baseUser);
+console.log("USER", user);
