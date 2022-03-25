c0ba7300148fc006df0c3d1a47b868e9baca40f0
Taha <>
2022-03-25 01:16:37 +0100

    Boucles

---
+++ ratt/boucles.js
@@ -0,0 +1,35 @@
+var arr = [10, 20, 30, 40];
+
+// for (var i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// var index = 0;
+// while (index < arr.length) {
+//     console.log(arr[index]);
+//     index++;
+// }
+
+// for (var i = arr.length - 1; i >= 0; i--) {
+//     console.log(arr[i]);
+// }
+
+// var index = arr.length - 1;
+// while (index >= 0) {
+//     console.log(arr[index]);
+//     index--;
+// }
+
+// var total = 5;
+// while (total > 0) {
+//     total--;
+//     console.log("Un utilisateur a quité la conversation! il reste : " + total);
+// }
+
+var inscriptions = 0;
+while (inscriptions < 10) {
+    console.log("Inscription" + (inscriptions + 1));
+    inscriptions++;
+}
+
+console.log("Inscription terminée");
