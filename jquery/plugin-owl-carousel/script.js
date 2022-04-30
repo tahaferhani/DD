e31c590b23934a4d78c7b76bcf15091e46c9a315
Taha <>
2022-04-30 16:44:40 +0000

    Jquery plugins

---
+++ jquery/plugin-owl-carousel/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 3,
+        margin: 20,
+        loop: true,
+        center: true,
+        nav: true,
+        dots: true,
+        smartSpeed: 1000,
+        autoplay: true,
+        autoplayTimeout: 2000
+    });
+});
